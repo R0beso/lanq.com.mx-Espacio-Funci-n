7161b51b15c89f2454f3aca36bad486b56f7c343
R0beso <jrobesoq@gmail.com>
2023-01-13 15:41:39 -0600

    .env created and dotenv deployed

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 
-const app = express()
-const port = 8081;
+const app = express();
+const port = process.env.PORT;
 
 
 app.use( express.static('public') );
